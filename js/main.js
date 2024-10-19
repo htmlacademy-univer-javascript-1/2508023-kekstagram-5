dd60c0cb8e59646e8aeee02f8eabed4780103139
Королева Дарья <darakoroleva599@gmail.com>
2024-10-19 14:07:35 +0500

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
-const names = ['keks', 'Саша', 'Ваня', 'Настя', 'Яна', 'Гриша', 'Никита', 'Андрей'];
-const messages = [
+const NAMES = ['keks', 'Саша', 'Ваня', 'Настя', 'Яна', 'Гриша', 'Никита', 'Андрей'];
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент'];
+
 //Генерирует случайное число из диапазона [a,b]
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
+
 //Создает комментарии к фотографиям
-function makeComments(){
+const makeComments = () => {
   const commentsCount = getRandomInteger(0,30);
   const comments = [];
   for (let i = 0; i < commentsCount; i++){
-    const name = names[getRandomInteger(0, names.length - 1)];
-    const message = messages[getRandomInteger(0, messages.length - 1)];
+    const name = NAMES[getRandomInteger(0, NAMES.length - 1)];
+    const message = MESSAGES[getRandomInteger(0, MESSAGES.length - 1)];
     const id = i + 1;
     comments.push({
       id: id,
@@ -29,10 +31,10 @@ function makeComments(){
     });
   }
   return comments;
-}
+};
 //Создает массив объектов
 // eslint-disable-next-line no-unused-vars
-const makePhotoArray = function() {
+const makePhotoArray = () => {
   const photos = [];
   for (let i = 1; i <= 25; i++){
     photos.push({
