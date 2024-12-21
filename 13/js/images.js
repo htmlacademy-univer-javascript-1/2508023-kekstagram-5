e3d10018ab689ac16760db0ecb2eabc14016f13b
Keks <keks@htmlacademy.ru>
2024-12-21 12:00:35 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/images.js
@@ -0,0 +1,19 @@
+import { pictures } from './api.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+const fragment = document.createDocumentFragment();
+
+pictures.forEach(({id, url, description, likes, comments}) => {
+  const picture = pictureTemplate.cloneNode(true);
+  const img = picture.querySelector('.picture__img');
+  img.id = id;
+  img.src = url;
+  img.alt = description;
+  picture.querySelector('.picture__likes').textContent = likes;
+  picture.querySelector('.picture__comments').textContent = comments.length;
+  fragment.appendChild(picture);
+});
+picturesContainer.appendChild(fragment);
+
+export { pictures };
